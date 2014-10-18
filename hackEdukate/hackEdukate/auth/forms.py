d64864c4b6e6ebf0ab922746682a9f1836a7222f
gzpgg3x <gzpgg3x@yahoo.com>
2014-10-18 14:34:44 -0400

    signinFormShorter
    signinFormShorter

--- hackEdukate/hackEdukate/auth/forms.py
+++ hackEdukate/hackEdukate/auth/forms.py
@@ -41,7 +41,8 @@ class SignUpForm(forms.ModelForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),
         max_length=30,
         required=True,
-        help_text='Usernames may contain <strong>alphanumeric</strong>, <strong>_</strong> and <strong>.</strong> characters')
+        # help_text='Usernames may contain <strong>alphanumeric</strong>, <strong>_</strong> and <strong>.</strong> characters')
+        help_text='')
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}), 
         label="Confirm your password",
